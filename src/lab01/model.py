caada11aa30db9c6960ae3fc7635fa0f7cdfa71f
Филипп <kofil@MacBook-Air.local>
2026-03-15 19:23:56 +0300

    Добавлена лабораторная работа 1

--- src/lab01/model.py
+++ src/lab01/model.py
@@ -1,13 +1,22 @@
+from validate import (
+    validate_title,
+    validate_author,
+    validate_year,
+    validate_pages,
+    validate_price
+)
+
+
 class Book:
     total_books = 0
 
     def __init__(self, title: str, author: str, year: int, pages: int, price: float):
-        # Валидация через отдельные методы
-        self._validate_title(title)
-        self._validate_author(author)
-        self._validate_year(year)
-        self._validate_pages(pages)
-        self._validate_price(price)
+
+        validate_title(title)
+        validate_author(author)
+        validate_year(year)
+        validate_pages(pages)
+        validate_price(price)
 
         self._title = title.strip()
         self._author = author.strip()
@@ -18,37 +27,6 @@ class Book:
 
         Book.total_books += 1
 
-    # ВАЛИДАЦИЯ
-
-    def _validate_title(self, value):
-        if not isinstance(value, str):
-            raise TypeError("Название должно быть строкой")
-        if not value.strip():
-            raise ValueError("Название должно быть непустой строкой")
-
-    def _validate_author(self, value):
-        if not isinstance(value, str):
-            raise TypeError("Автор должен быть строкой")
-        if not value.strip():
-            raise ValueError("Автор должен быть непустой строкой")
-
-    def _validate_year(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Год издания должен быть целым числом")
-        if value < 1450 or value > 2026:
-            raise ValueError("Год издания должен быть в диапазоне от 1450 до 2026")
-
-    def _validate_pages(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Количество страниц должно быть целым числом")
-        if value <= 0:
-            raise ValueError("Количество страниц должно быть больше 0")
-
-    def _validate_price(self, value):
-        if not isinstance(value, (int, float)):
-            raise TypeError("Цена должна быть числом")
-        if value < 0:
-            raise ValueError("Цена не может быть отрицательной")
 
     # СВОЙСТВА
 
@@ -74,14 +52,15 @@ class Book:
 
     @price.setter
     def price(self, value):
-        self._validate_price(value)
+        validate_price(value)
         self._price = float(value)
 
     @property
     def is_available(self):
         return self._is_available
 
-    # ЛОГИЧЕСКОЕ СОСТОЯНИЕ
+
+    # СОСТОЯНИЕ
 
     def borrow(self):
         if not self._is_available:
@@ -93,6 +72,7 @@ class Book:
             raise ValueError("Книга уже находится в библиотеке")
         self._is_available = True
 
+
     # БИЗНЕС-МЕТОДЫ
 
     def discount(self, percent: float):
@@ -100,10 +80,12 @@ class Book:
             raise TypeError("Скидка должна быть числом")
         if percent < 0 or percent > 100:
             raise ValueError("Скидка должна быть от 0 до 100")
+
         self._price *= (1 - percent / 100)
 
     def is_classic(self):
         return self._year < 1970
+
 
     # МАГИЧЕСКИЕ МЕТОДЫ
 
